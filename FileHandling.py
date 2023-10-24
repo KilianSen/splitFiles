a08871ed0fcaf199a88b48a09066e86e85090e79
kilian <kilian@senger.group>
2023-10-24 17:38:43 +0200

    Add optional compression to archive creation and extraction
    Added two optional parameters, `compress_whole` and `compress_chunks`, to the `create_archive` and `dearchive` functions in FileHandling.py, and reflected these changes to the main.py. If set to True, these parameters enable compression on the whole file and for each chunk during archival and dearchival processes, providing users greater flexibility in terms of compression needs. Default behavior remains as compressing the whole file and every chunk, preserving backward compatibility.

--- FileHandling.py
+++ FileHandling.py
@@ -6,18 +6,27 @@ import hashlib
 import os
 
 
-def create_archive(file: str, output: str, max_size: int = 20):
+def create_archive(file: str, output: str, max_size: int = 20,
+                   compress_whole: bool = True, compress_chunks: bool = True):
     max_target_size: int = max_size * 1024 * 1024
     with tempfile.TemporaryFile() as temporary:
         with open(file, "rb") as f:
             file_data = f.read()
-            temporary.write(bz2.compress(
-                os.path.basename(file).encode() + b'_.COM.!' + hashlib.sha1(file_data).hexdigest().encode() + b'_.COM.!'
-                + file_data, 9))
+
+            if compress_whole:
+                temporary.write(bz2.compress(
+                    os.path.basename(file).encode() + b'_.COM.!' + hashlib.sha1(file_data).hexdigest().encode() + b'_.COM.!'
+                    + file_data, 9))
+            else:
+                temporary.write(
+                    os.path.basename(file).encode() + b'_.COM.!' + hashlib.sha1(
+                        file_data).hexdigest().encode() + b'_.COM.!'
+                    + file_data)
+
         temporary.seek(0)
         chunk_counter = 0
         while chunk := temporary.read(max_target_size):
-            compressed_chunk: bytes = lzma.compress(chunk, lzma.FORMAT_XZ)
+            compressed_chunk: bytes = lzma.compress(chunk, lzma.FORMAT_XZ) if compress_chunks else chunk
             with open(
                     f"{output}\\{
                     hashlib.sha1(file.encode()).hexdigest()[:6]
@@ -28,7 +37,7 @@ def create_archive(file: str, output: str, max_size: int = 20):
             chunk_counter += 1
 
 
-def dearchive(directory: str):
+def dearchive(directory: str, compress_whole: bool = True, compress_chunks: bool = True):
     files = os.listdir(directory)
     compatible_files = {}
     for file in files:
@@ -47,11 +56,11 @@ def dearchive(directory: str):
                 file = working_file[str(i).rjust(4, "0")]
 
                 with open(directory + '\\' + file, 'rb') as f:
-                    w.write(lzma.decompress(f.read()))
+                    w.write(lzma.decompress(f.read()) if compress_chunks else f.read())
 
             w.seek(0)
 
-            reconstructed_data = bz2.decompress(w.read())
+            reconstructed_data = bz2.decompress(w.read()) if compress_whole else w.read()
             file_name = reconstructed_data.split(b'_.COM.!')[0]
             saved_hash = reconstructed_data.split(b'_.COM.!')[1]
             reconstructed_data = reconstructed_data.split(b'_.COM.!', 2)[2]
--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import sys
 from FileHandling import create_archive, dearchive
 
 
-def process_files(mode, input_path: str, output_path: str = None, max_size: int = None):
+def process_files(mode, input_path: str, output_path: str = None, max_size: int = None,
+                  compress_whole: bool = True, compress_chunks: bool = True):
     """
     Process files based on the mode of operation
     :param mode: Defines the type of operation to perform: archive, dearchive, auto
     :param input_path: Path to file to archive or directory to dearchive
     :param output_path: Path where to save the archive
     :param max_size: Max chunk size in megabytes
+    :param compress_whole: Enable compression of the whole file before the archival.
+    This value has to be the same for dearchiving.
+    :param compress_chunks:Enable compression of the chunk files during archival.
+    This value has to be the same for dearchiving.
     """
     if not os.path.exists(input_path):
         raise ValueError(f'Input path {input_path} does not exist')
@@ -24,25 +29,37 @@ def process_files(mode, input_path: str, output_path: str = None, max_size: int 
     if mode == 'archive':
         if not output_path or not max_size:
             raise ValueError('For "archive" mode, both "output_path" and "max_size" parameters are required')
-        create_archive(input_path, output_path, max_size)
+        create_archive(input_path, output_path, max_size, compress_whole, compress_chunks)
     elif mode == 'dearchive':
-        dearchive(input_path)
+        dearchive(input_path, compress_whole, compress_chunks)
     else:
         raise ValueError('Invalid mode!')
 
 
 def main():
     parser = argparse.ArgumentParser(description='Process some parameters.')
+
     parser.add_argument('-m', '--mode', type=str, default='auto',
                         help='Defines the type of operation to perform: archive, dearchive, auto')
-    parser.add_argument('-i', '--input_path', type=str, required=True, help='Path to file to archive or directory to dearchive')
-    parser.add_argument('-o', '--output_path', type=str, default=None, help='Path where to save the archive')
-    parser.add_argument('-s', '--max_size', type=int, default=15, help='Max chunk size in whole megabytes')
+    parser.add_argument('-i', '--input_path', type=str, required=True,
+                        help='Path to file to archive or directory to dearchive')
+    parser.add_argument('-o', '--output_path', type=str, default=None,
+                        help='Path where to save the archive')
+    parser.add_argument('-s', '--max_size', type=int, default=15,
+                        help='Max chunk size in whole megabytes')
+
+    parser.add_argument('-c1', '--compress_whole', type=bool, default=True,
+                        help='Enable compression of the whole file before the archival.'
+                             'This value has to be the same for dearchiving.')
+    parser.add_argument('-c2', '--compress_chunk', type=bool, default=True,
+                        help='Enable compression of the chunk files during archival.'
+                             'This value has to be the same for dearchiving.')
 
     args = parser.parse_args()
 
     try:
-        process_files(args.mode, args.input_path, args.output_path, args.max_size)
+        process_files(args.mode, args.input_path, args.output_path, args.max_size,
+                      args.compress_whole, args.compress_chunk)
     except ValueError as err:
         print(f"Error: {err}", file=sys.stderr)
         sys.exit(1)
