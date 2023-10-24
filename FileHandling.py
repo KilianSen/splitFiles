14f3bdb926a9589c06d6d5fee323a0178be424b8
kilian <kilian@senger.group>
2023-10-24 17:01:17 +0200

    Initial commit

---
+++ FileHandling.py
@@ -0,0 +1,67 @@
+import lzma
+import bz2
+import pathlib
+import tempfile
+import hashlib
+import os
+
+
+def create_archive(file: str, output: str, max_size: int = 20):
+    max_target_size: int = max_size * 1024 * 1024
+    with tempfile.TemporaryFile() as temporary:
+        with open(file, "rb") as f:
+            file_data = f.read()
+            temporary.write(bz2.compress(
+                os.path.basename(file).encode() + b'_.COM.!' + hashlib.sha1(file_data).hexdigest().encode() + b'_.COM.!'
+                + file_data, 9))
+        temporary.seek(0)
+        chunk_counter = 0
+        while chunk := temporary.read(max_target_size):
+            compressed_chunk: bytes = lzma.compress(chunk, lzma.FORMAT_XZ)
+            with open(
+                    f"{output}\\{
+                    hashlib.sha1(file.encode()).hexdigest()[:6]
+                    }{
+                    chunk_counter.__str__().rjust(4, "0")
+                    }{hashlib.sha1(chunk).hexdigest()[:6:]}.bin.xz.part", 'wb') as cf2:
+                cf2.write(compressed_chunk)
+            chunk_counter += 1
+
+
+def dearchive(directory: str):
+    files = os.listdir(directory)
+    compatible_files = {}
+    for file in files:
+        if file.endswith('.bin.xz.part'):
+            id = file[:6]
+            num = file[6:10]
+            if id not in compatible_files:
+                compatible_files[id] = {}
+            compatible_files[id][num] = file
+
+    for file_key, _ in compatible_files.items():
+        working_file = compatible_files[file_key]
+
+        with tempfile.TemporaryFile() as w:
+            for i in range(len(working_file.keys())):
+                file = working_file[str(i).rjust(4, "0")]
+
+                with open(directory + '\\' + file, 'rb') as f:
+                    w.write(lzma.decompress(f.read()))
+
+            w.seek(0)
+
+            reconstructed_data = bz2.decompress(w.read())
+            file_name = reconstructed_data.split(b'_.COM.!')[0]
+            saved_hash = reconstructed_data.split(b'_.COM.!')[1]
+            reconstructed_data = reconstructed_data.split(b'_.COM.!', 2)[2]
+            if hashlib.sha1(reconstructed_data).hexdigest() == saved_hash.decode():
+                with open(directory + '\\' + file_name.decode(), 'wb') as wnf:
+                    wnf.write(reconstructed_data)
+
+                    for i in range(len(working_file.keys())):
+                        file = working_file[str(i).rjust(4, "0")]
+                        os.remove(directory + '\\' + file)
+                    print(f'Successfully reconstructed file {directory + '\\' + file_name.decode()}')
+            else:
+                print('Reconstructed file hash does not match with the saved hash! Reconstruction halted!')
---
+++ main.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+import sys
+from FileHandling import create_archive, dearchive
+
+
+def process_files(mode, input_path: str, output_path: str = None, max_size: int = None):
+    """
+    Process files based on the mode of operation
+    :param mode: Defines the type of operation to perform: archive, dearchive, auto
+    :param input_path: Path to file to archive or directory to dearchive
+    :param output_path: Path where to save the archive
+    :param max_size: Max chunk size in megabytes
+    """
+    if not os.path.exists(input_path):
+        raise ValueError(f'Input path {input_path} does not exist')
+
+    if mode == 'auto':
+        mode = 'archive' if os.path.isfile(input_path) else 'dearchive'
+
+    if output_path and not os.path.isdir(os.path.dirname(output_path)):
+        raise ValueError(f'Output path {output_path} directory does not exist')
+
+    if mode == 'archive':
+        if not output_path or not max_size:
+            raise ValueError('For "archive" mode, both "output_path" and "max_size" parameters are required')
+        create_archive(input_path, output_path, max_size)
+    elif mode == 'dearchive':
+        dearchive(input_path)
+    else:
+        raise ValueError('Invalid mode!')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some parameters.')
+    parser.add_argument('-m', '--mode', type=str, default='auto',
+                        help='Defines the type of operation to perform: archive, dearchive, auto')
+    parser.add_argument('-i', '--input_path', type=str, required=True, help='Path to file to archive or directory to dearchive')
+    parser.add_argument('-o', '--output_path', type=str, default=None, help='Path where to save the archive')
+    parser.add_argument('-s', '--max_size', type=int, default=15, help='Max chunk size in whole megabytes')
+
+    args = parser.parse_args()
+
+    try:
+        process_files(args.mode, args.input_path, args.output_path, args.max_size)
+    except ValueError as err:
+        print(f"Error: {err}", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
